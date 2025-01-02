8a0738a15a597dc57eae1f34a9a5d62058542db2
Rostik <across2008@ukr.net>
2025-01-02 16:08:08 +0200

    Навчання JavaScript

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// alert (" Hello World! ");
+//let name = "Roland";
+//let age = 34;
+//let myColor = "black";
+//console.log(name + " " + age+ " " + myColor);
+
+let num1 =10;
+let num2 =5;
+
+console.log(num1+num2-num2/num2);
+
+const page1 = 50;
+const page2 = 150;
+const result = `Сума чисел: ${page1} та ${page2} буде дорівнювати ${page1 + page2}.
+А якщо ми ${page2} розділемо на 2 то у нас вийде ${page2/2} `;
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
